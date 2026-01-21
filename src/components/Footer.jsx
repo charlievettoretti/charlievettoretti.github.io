97fefb143c729ab93ace80c4800deff215386ebd
Charlie Vettoretti <charliev15@gmail.com>
2026-01-20 23:37:08 -0500

    Fixed minor design issues. Added footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import styles from './resources/Footer.module.css';
+
+function Footer() {
+
+    return (
+        <div className={styles.container}>
+            <p>2026 Charlie Vettoretti</p>
+            <p>Email: charliev15@gmail.com</p>
+        </div>
+    )
+};
+
+export default Footer;
--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -5,18 +5,33 @@ import ProjectSection from './Projects/ProjectSection';
 import ProgrammingLanguages from './ProgrammingLanguages/ProgrammingLanguages';
 import SoftwareSkills from './SoftwareSkills/SoftwareSkills';
 import RelevantCoursework from './RelevantCoursework/RelevantCoursework';
+import Footer from './Footer';
+
+import CharliePicture from './resources/images/charlie_photo.png';
 
 function HomePage() {
     return (
         <div className={styles.background}>
-            <div className={styles.aboutMe}>
+            <div id='aboutme' className={styles.aboutMe}>
                 <h3>About Me:</h3>
+                <div className={styles.aboutMeGroup}>
+                    <img src={CharliePicture} className={styles.photo} />
+                    <p className={styles.aboutMeText}>
+                        I’m a Computer Engineering graduate with a strong interest in building full stack applications 
+                        and working with AI. I primarily work with React, Node.js, and PostgreSQL, and I enjoy 
+                        creating systems that are both functional and user focused. Lately, I’ve been exploring how 
+                        large language models and AI tools can be integrated into real applications to improve workflows 
+                        and automate processes. I’m highly self motivated, always learning new technologies, and 
+                        focused on continuously improving the quality of what I build.
+                    </p>
+                </div>
             </div>
             <RelevantExperience />
             <ProjectSection />
             <ProgrammingLanguages />
             <SoftwareSkills />
             <RelevantCoursework />
+            <Footer />
         </div>
     );
 };
